d3ed9a867bbba95bfe9e1ad6088696ca9351e600
romainschert <150144520+romainschert@users.noreply.github.com>
2025-05-07 15:20:02 +0200

    Update docker-entrypoint.sh

--- app-Flash-Card-adonisjs/docker-entrypoint.sh
+++ app-Flash-Card-adonisjs/docker-entrypoint.sh
@@ -8,12 +8,12 @@ sleep 10
 
 
 # Exécuter les migrations
-echo "Exécution des migrations..."
-node ace migration:run --force
+#echo "Exécution des migrations..."
+#node ace migration:run --force
 
 # Exécuter les seeds
-echo "Exécution des seeds..."
-node ace db:seed
+#echo "Exécution des seeds..."
+#node ace db:seed
 
 # Démarrer l'application
 echo "Démarrage de l'application..."
