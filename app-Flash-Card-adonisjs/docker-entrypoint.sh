1ac58753aa2a045d6d13df1956cf37faa0b66e01
romainschert <150144520+romainschert@users.noreply.github.com>
2025-05-14 13:48:55 +0200

    production

--- app-Flash-Card-adonisjs/docker-entrypoint.sh
+++ app-Flash-Card-adonisjs/docker-entrypoint.sh
@@ -7,5 +7,11 @@ sleep 10
 echo "Build AdonisJS (bin/server.js)..."
 node ace build --ignore-ts-errors
 
+# Vérification de la présence du fichier server.js avant démarrage
+if [ ! -f "bin/server.js" ]; then
+  echo "Erreur : Le fichier bin/server.js est introuvable"
+  exit 1
+fi
+
 echo "Démarrage de l'application..."
 node bin/server.js
