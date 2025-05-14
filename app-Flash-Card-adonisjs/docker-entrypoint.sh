ec321f9a8755fe1eee9a1c39148617b6a31a66f3
romainschert <150144520+romainschert@users.noreply.github.com>
2025-05-14 14:40:04 +0200

    production 7

--- app-Flash-Card-adonisjs/docker-entrypoint.sh
+++ app-Flash-Card-adonisjs/docker-entrypoint.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 set -e
 
-# Attente du démarrage de MySQL
 echo "Attente du démarrage de MySQL..."
 sleep 10
 
-# Vérifiez si le fichier bin/server.js existe après compilation
-if [ ! -f "/app/bin/server.js" ]; then
-  echo "Erreur : le fichier bin/server.js est introuvable."
-  exit 1
-fi
+echo "Build AdonisJS (bin/server.js)..."
+node ace build --ignore-ts-errors
 
-# Démarrer l'application
 echo "Démarrage de l'application..."
 node bin/server.js
