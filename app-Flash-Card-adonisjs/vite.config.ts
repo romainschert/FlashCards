913e3a4495d7e0693dda4432fc8824259b29d9a9
romainschert <150144520+romainschert@users.noreply.github.com>
2025-05-14 15:54:26 +0200

    production 12

--- app-Flash-Card-adonisjs/vite.config.ts
+++ app-Flash-Card-adonisjs/vite.config.ts
@@ -1,33 +1,14 @@
 import { defineConfig } from 'vite'
 import adonisjs from '@adonisjs/vite/client'
-import path from 'path'
-import fs from 'fs'
+import glob from 'fast-glob'
 
-// Fonction pour récupérer tous les fichiers CSS dans le dossier 'resources/css'
-function getCssFiles() {
-  const cssDir = path.resolve(__dirname, 'resources/css')
-  const files = fs.readdirSync(cssDir)
-
-  return files
-    .filter((file) => file.endsWith('.css')) // Filtrer pour ne garder que les fichiers .css
-    .map((file) => path.resolve(cssDir, file)) // Créer un tableau avec les chemins complets des fichiers CSS
-}
+const cssFiles = glob.sync('resources/css/*.css')
+const jsFiles = glob.sync('resources/js/*.js')
 
 export default defineConfig({
   plugins: [
     adonisjs({
-      /**
-       * Entrypoints de l'application. Chaque entrée crée un bundle distinct.
-       * Inclure tous les fichiers CSS dans 'resources/css'
-       */
-      entrypoints: [
-        ...getCssFiles(), // Récupérer tous les fichiers CSS
-        'resources/js/app.js', // Ajouter le fichier JavaScript principal
-      ],
-
-      /**
-       * Dossiers à surveiller et recharger le navigateur lors de changements de fichiers
-       */
+      entrypoints: [...cssFiles, ...jsFiles],
       reload: ['resources/views/**/*.edge'],
     }),
   ],
