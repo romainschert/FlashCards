39be84de98a2fc1119fc268fe17f946246da9c91
romainschert <150144520+romainschert@users.noreply.github.com>
2025-05-21 16:11:02 +0200

    production 22

--- app-Flash-Card-adonisjs/vite.config.ts
+++ app-Flash-Card-adonisjs/vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite'
 import adonisjs from '@adonisjs/vite/client'
-import glob from 'fast-glob'
-
-const cssFiles = glob.sync('resources/css/*.css')
-const jsFiles = glob.sync('resources/js/*.js')
 
 export default defineConfig({
   plugins: [
     adonisjs({
-      entrypoints: [...cssFiles, ...jsFiles],
+      entrypoints: [
+        'resources/css/app.css',
+        'resources/css/decks_created.css',
+        'resources/css/decks.css',
+        'resources/css/flashcard-detail.css',
+        'resources/css/flashcards.css',
+        'resources/css/flashcards_create.css',
+        'resources/css/home.css',
+        'resources/css/login.css',
+        'resources/css/register.css',
+        'resources/js/app.js',
+      ],
       reload: ['resources/views/**/*.edge'],
     }),
   ],
