b47b439c760706ebb8ab8088898957f4f4a331c6
romainschert <150144520+romainschert@users.noreply.github.com>
2025-05-21 16:23:55 +0200

    production 23

--- app-Flash-Card-adonisjs/config/database.ts
+++ app-Flash-Card-adonisjs/config/database.ts
@@ -2,15 +2,15 @@ import env from '#start/env'
 import { defineConfig } from '@adonisjs/lucid'
 
 const dbConfig = defineConfig({
-  connection: 'mysql',
+  connection: 'pg', // Nom de la connexion par défaut
   connections: {
-    mysql: {
-      client: 'mysql2',
+    pg: {
+      client: 'pg', // Utilise le client PostgreSQL
       connection: {
         host: env.get('DB_HOST'),
         port: env.get('DB_PORT'),
         user: env.get('DB_USER'),
-        password: env.get('DB_PASSWORD'),
+        password: env.get('DB_PASSWORD', ''),
         database: env.get('DB_DATABASE'),
       },
       migrations: {
