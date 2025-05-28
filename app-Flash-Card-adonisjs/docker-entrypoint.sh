244dca98c02388b8430dfcd542d8ae869a31c1ce
romainschert <150144520+romainschert@users.noreply.github.com>
2025-05-28 13:41:54 +0200

    production 27

--- app-Flash-Card-adonisjs/docker-entrypoint.sh
+++ app-Flash-Card-adonisjs/docker-entrypoint.sh
@@ -4,8 +4,19 @@ set -e
 echo "Attente du démarrage de MySQL..."
 sleep 10
 
-echo "Build AdonisJS (bin/server.js)..."
-node ace build --ignore-ts-errors
+# Exécuter les migrations
+echo "Exécution des migrations..."
+node ace migration:run --force
 
+# Exécuter les seeds
+echo "Exécution des seeds..."
+node ace db:seed
+
+# Démarrer l'application
 echo "Démarrage de l'application..."
+npm run build
+npm ci --omit='dev'
+mkdir ./build/resources/css
+cp ./resources/css/app.css ./build/resources/css/app.css
+cd build
 node bin/server.js
